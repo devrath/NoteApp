1dc5c0b5c69ef843b2f936bef60f96608a2c37df
Devrath <devrath.dev595@gmail.com>
2023-02-05 19:22:07 +0530

    note item added

--- Code/app/src/main/java/com/istudio/notes/feature_note/presentation/notes/components/NoteItem.kt
+++ Code/app/src/main/java/com/istudio/notes/feature_note/presentation/notes/components/NoteItem.kt
@@ -3,18 +3,26 @@ package com.istudio.notes.feature_note.presentation.notes.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -26,45 +34,53 @@ import com.istudio.notes.feature_note.domain.model.Note
 fun NoteItem(
     note: Note,
     modifier: Modifier = Modifier,
-    cornerRadius : Dp = 10.dp,
-    cutCornerSize : Dp = 30.dp
+    cornerRadius : Dp = 10.dp
 ) {
 
-    Row(
+    Box(
         modifier = modifier
+            .clip(shape = RoundedCornerShape(cornerRadius))
+            .background(MaterialTheme.colors.background)
             .fillMaxWidth()
-            .height(300.dp)
+            .height(100.dp)
+            .padding(10.dp)
     ) {
-
-        Column(
-            modifier = modifier
-                .background(Color.Cyan)
-                .weight(1F, fill = true),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.Start
-        ) {
-            Text(
-                text = note.title,
-                fontSize = 18.sp
-            )
-            Text(
-                text = note.content,
-                fontSize = 12.sp
-            )
-        }
-
-        Column(
-            modifier = modifier.background(Color.Blue),
-            verticalArrangement = Arrangement.Bottom,
-            horizontalAlignment = Alignment.Start
+        Row(
+            modifier = modifier.fillMaxWidth()
         ) {
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_delete),
-                contentDescription = stringResource(id = R.string.str_delete)
-            )
-        }
+            Column(
+                modifier = modifier.weight(1F, fill = true),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    modifier = modifier.padding(bottom = 5.dp),
+                    text = note.title,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = note.content,
+                    fontSize = 12.sp,
+                    maxLines = 8,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
 
+            Column(
+                modifier = modifier.fillMaxHeight().padding(start = 10.dp),
+                verticalArrangement = Arrangement.Bottom,
+                horizontalAlignment = Alignment.Start
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.ic_delete),
+                    contentDescription = stringResource(id = R.string.str_delete)
+                )
+            }
+
+        }
     }
 
 }
@@ -73,12 +89,12 @@ fun NoteItem(
 @Preview
 @Composable
 private fun CurrentScreen() {
-    /*NoteItem(
+    NoteItem(
         note = Note(
             id = 1 , title = "Title" ,
             content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             timestamp = 20
         )
-    )*/
+    )
 }
 
